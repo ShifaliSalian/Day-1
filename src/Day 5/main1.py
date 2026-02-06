17b73dfadbb3f143eb65c12e4b80a69364d81799
ShifaliSalian <shifaliksalian01@gmail.com>
2026-02-06 14:50:24 +0530

    Add files via upload

---
+++ src/Day 5/main1.py
@@ -0,0 +1,8 @@
+
+#Task-2 main.py
+import math_operations
+result_power = math_operations.power(2, 10)
+print("2^10 =", result_power)
+nums = [10, 20, 30, 40]
+result_avg = math_operations.average(nums)
+print("Average =", result_avg)
---
+++ src/Day 5/math_operations.py
@@ -0,0 +1,6 @@
+#Task-2 math_operations.py
+def power(base,exp):
+    return base**exp
+
+def average(numbers_list):
+    return sum(numbers_list)/len(numbers_list)
